2b273b0f11b4bfae2f7b5bf5fd1d80f70ee5efa2
HamedGh <h.ghadirian@outlook.com>
2016-01-18 15:48:45 +0330

    base functionality prepared.

--- app/src/main/java/hamed_gh/ir/okdonwloadmanager/DownloadService.java
+++ app/src/main/java/hamed_gh/ir/okdonwloadmanager/DownloadService.java
@@ -12,15 +12,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import okio.BufferedSink;
-import okio.BufferedSource;
-import okio.Okio;
 
 public class DownloadService extends Service {
     private final HashMap<String, String> headers;
@@ -29,11 +28,11 @@ public class DownloadService extends Service {
     ProgressBar progressBar;
     TextView textView;
     File sdCard = Environment.getExternalStorageDirectory();
-    File dir = new File(sdCard.getAbsolutePath() + "/ir.hamed_gh.download/");
-    double fileSize = 0;
+    File dir;
     public static final int DOWNLOAD_CHUNK_SIZE = 2048; //Same as Okio Segment.SIZE
-
     DownloadAsyncTask dat;
+
+    private String TAG = "DownloadService";
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -41,11 +40,13 @@ public class DownloadService extends Service {
     }
 
     public DownloadService(String url,
+                           String path,
                            HashMap<String, String> headers,
                            Context c,
                            ProgressBar pBar,
                            TextView tv) {
 
+        dir = new File(path);
         if (!dir.exists()) {
             dir.mkdirs();
         }
@@ -72,6 +73,10 @@ public class DownloadService extends Service {
             Log.d("status", "download is incomplete, filesize:" + from.length());
             return from.length();
         }
+        from = new File(dir,fileName);
+        if (from.exists()){
+            return -1;
+        }
         return 0;
     }
 
@@ -89,44 +94,106 @@ public class DownloadService extends Service {
             }
             try {
                 OkHttpClient client = new OkHttpClient();
+
                 Request.Builder builder = new Request.Builder()
-                        .url(downloadUrl)
-                        .addHeader("token", "12-c5c856982961f5ec59cad075015409f17c2a1b99")
-                        .addHeader("uuid", "1")
-                        .addHeader("agent", "postman");
+                        .url(downloadUrl);
 
                 for (String key : headers.keySet()) {
                     builder.addHeader(key, headers.get(key));
                 }
+
+                long downloaded = isIncomplete();
+                if (downloaded > 0) {
+                    builder.addHeader("Range", "bytes=" + (downloaded) + "-");
+                } else if (downloaded == -1) {
+                    return "Download compeleted.";
+                }
+
+                Log.d(TAG, "doInBackground: downloaded= " + downloaded);
+
                 Request request = builder.build();
                 Response response = client.newCall(request).execute();
+                Log.i(TAG, "doInBackground: response.body=" + response.body().contentLength());
 
-                File f = new File(dir, "dummy-incomplete");
+                ResponseBody body = response.body();
+                long contentLength = body.contentLength() + downloaded;
+                Log.d(TAG, "doInBackground: contentLength= " + contentLength);
+
+                long bytesRead = downloaded;
+                Log.d(TAG, "doInBackground: bytesRead= " + bytesRead);
+                int bufferLength = 0;
+
+                byte[] buffer = new byte[DOWNLOAD_CHUNK_SIZE];
+                FileOutputStream fos = new FileOutputStream(dir.getAbsolutePath() + "/" + fileName + "-incomplete", true);
+                InputStream inputStream = response.body().byteStream();
+
+//                        response.getInputStream();
+                while ((bufferLength = inputStream.read(buffer)) != -1) {
+                    if (isCancelled()) {
+                        break;
+                    }
+                    fos.write(buffer, 0, bufferLength);
+
+                    bytesRead += bufferLength;
+                    int progress = (int) ((bytesRead * 100) / contentLength);
+                    Log.d(TAG, "doInBackground: bytesRead= " + bytesRead);
+                    Log.d(TAG, "doInBackground: contentLength= " + contentLength);
+                    Log.d(TAG, "doInBackground: progress= " + progress);
+
+                    publishProgress(progress);
+
+//                    downloadedSize += bufferLength;
+//                    percentage = (int) ((downloadedSize / fileSize) * 100);
+//                    publishProgress(percentage);
+                    //Log.d("status","downloading: " + downloadedSize+"/"+fileSize+" ("+percentage+"%)");
+                }
+                fos.close();
 
 //                BufferedSink sink = Okio.buffer(Okio.sink(f));
 //                sink.writeAll(response.body().source());
 //                sink.close();
 
-                ResponseBody body = response.body();
-                long contentLength = body.contentLength();
-                BufferedSource source = body.source();
+//                bytesRead = 0;
+//                while (source.read(sink.buffer(), DOWNLOAD_CHUNK_SIZE) != -1) {
+//                    if(isCancelled()){
+//                        break;
+//                    }
+//                    bytesRead += DOWNLOAD_CHUNK_SIZE;
+//                    int progress = (int) ((bytesRead * 100) / contentLength);
+//                    publishProgress(progress);
+//                }
+//                sink.writeAll(source);
+//                sink.close();
 
-                BufferedSink sink = Okio.buffer(Okio.sink(f));
 
-                long bytesRead = 0;
-                while (source.read(sink.buffer(), DOWNLOAD_CHUNK_SIZE) != -1) {
-                    if(isCancelled()){
-                        break;
-                    }
-                    bytesRead += DOWNLOAD_CHUNK_SIZE;
-                    int progress = (int) ((bytesRead * 100) / contentLength);
-                    publishProgress(progress);
-                }
-                sink.writeAll(source);
-                sink.close();
+//                File f = new File(dir, fileName + "-incomplete");
+//                BufferedSource source = body.source();
+//                BufferedSink sink;
+//                if (f.exists()) {
+//                    sink = Okio.buffer(Okio.appendingSink(f));
+//                }else {
+//                    sink = Okio.buffer(Okio.sink(f));
+//                }
+//                byte[] buf=new byte[1024];
+//                while ((bufferLength = source.read(buf)) != -1) {
+//                    if(isCancelled()){
+//                        break;
+//                    }
+//                    bytesRead += bufferLength;
+//                    sink.write(buf,0, bufferLength);
+//
+//                    sink.flush();
+//                    int progress = (int) ((bytesRead * 100) / contentLength);
+//                    Log.d(TAG, "doInBackground: bytesRead= " + bytesRead);
+//                    Log.d(TAG, "doInBackground: contentLength= "+ contentLength);
+//                    Log.d(TAG, "doInBackground: progress= "+ progress);
+//
+//                    publishProgress(progress);
+//                }
+//                sink.close();
 
             } catch (Exception e) {
-                Log.e("Download Failed", "Error: " + e.getMessage());
+                Log.e(TAG, "Error: " + e.getMessage());
             }
             if (isCancelled()) {
                 return "Download cancelled!";
@@ -140,20 +207,19 @@ public class DownloadService extends Service {
             if (progressBar != null) {
                 progressBar.setProgress(values[0]);
             } else {
-                Log.w("status", "ProgressBar is null, please supply one!");
+                Log.w(TAG, "ProgressBar is null, please supply one!");
             }
 
             if (textView != null){
                 textView.setText("%" + values[0]);
             } else {
-                Log.d("status", "Textview is null, please supply one!");
+                Log.d(TAG, "Textview is null, please supply one!");
             }
-
         }
 
         @Override
         protected void onPreExecute() {
-            mLocalBroadcastManager.sendBroadcast(new Intent("org.test.download.DOWNLOAD_STARTED"));
+            mLocalBroadcastManager.sendBroadcast(new Intent("ir.hamed_gh.download.DOWNLOAD_STARTED"));
         }
 
         @Override
@@ -161,12 +227,12 @@ public class DownloadService extends Service {
             File from = new File(dir, fileName + "-incomplete");
             File to = new File(dir, fileName);
             from.renameTo(to);
-            mLocalBroadcastManager.sendBroadcast(new Intent("org.test.download.DOWNLOAD_FINISHED"));
+            mLocalBroadcastManager.sendBroadcast(new Intent("ir.hamed_gh.download.DOWNLOAD_FINISHED"));
         }
 
         @Override
         protected void onCancelled() {
-            mLocalBroadcastManager.sendBroadcast(new Intent("org.test.download.DOWNLOAD_CANCELLED"));
+            mLocalBroadcastManager.sendBroadcast(new Intent("ir.hamed_gh.download.DOWNLOAD_CANCELLED"));
         }
     }
 }
--- app/src/main/java/hamed_gh/ir/okdonwloadmanager/MainActivity.java
+++ app/src/main/java/hamed_gh/ir/okdonwloadmanager/MainActivity.java
@@ -1,13 +1,24 @@
 package hamed_gh.ir.okdonwloadmanager;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import net.rdrei.android.dirchooser.DirectoryChooserActivity;
+import net.rdrei.android.dirchooser.DirectoryChooserConfig;
+
+import java.io.File;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
     private DownloadService ds;
     private Button btnStart;
     private Button btnPause;
+    private LocalBroadcastManager mLocalBroadcastManager;
+
+    //In an Activity
+    private String[] mFileList;
+    private File mPath = new File(Environment.getExternalStorageDirectory() + "");// + "//yourdir//");
+    private String mChosenFile;
+    private static final String FTYPE = ".txt";
+    private static final int DIALOG_LOAD_FILE = 1000;
+    private String TAG = "MainActivity";
+    private Button btnBrowse;
+    private TextView precentTextview;
+    private ProgressBar progressBar;
+    private TextView downloadLinkTextView;
+    private TextView pathTextview;
+    String path;
+    private int REQUEST_DIRECTORY;
+
+    private void findViews(){
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnBrowse = (Button) findViewById(R.id.browse_btn);
+
+        precentTextview = (TextView) findViewById(R.id.precent_textview);
+        pathTextview = (TextView) findViewById(R.id.path_textview);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        downloadLinkTextView = (TextView) findViewById(R.id.downloadLink_TextView);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +62,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ctx = this;
-        final TextView tv = (TextView)findViewById(R.id.textview);
-        final ProgressBar pb = (ProgressBar)findViewById(R.id.progressbar);
+        findViews();
+        path = Environment.getExternalStorageDirectory()+"";
+        pathTextview.setText(path);
 
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnPause = (Button) findViewById(R.id.btnPause);
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent chooserIntent = new Intent(MainActivity.this, DirectoryChooserActivity.class);
 
-        urlString = "http://api.irangrammy.com/api/v4/en/music/11238-5e570e44ae82f0f77133aad4f7789078";
-        final HashMap<String,String> headers = new HashMap<>();
-        headers.put("token","12-c5c856982961f5ec59cad075015409f17c2a1b99");
-        headers.put("uuid","1");
+                final DirectoryChooserConfig config = DirectoryChooserConfig.builder()
+                        .newDirectoryName("DirChooserSample")
+                        .allowReadOnlyDirectory(true)
+                        .allowNewDirectoryNameModification(true)
+                        .build();
+
+                chooserIntent.putExtra(DirectoryChooserActivity.EXTRA_CONFIG, config);
+
+                // REQUEST_DIRECTORY is a constant integer to identify the request, e.g. 0
+                REQUEST_DIRECTORY = 0;
+                startActivityForResult(chooserIntent, REQUEST_DIRECTORY);
+
+//                loadFileList();
+//                onCreateDialog();
+
+//                File mPath = new File(path);// + "//DIR//");
+//                FileDialog fileDialog = new FileDialog(MainActivity.this, mPath);
+////                fileDialog.setFileEndsWith(".txt");
+//                fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+//                    public void fileSelected(File file) {
+//                        Log.d(getClass().getName(), "selected file " + file.toString());
+//                    }
+//                });
+
+                //fileDialog.addDirectoryListener(new FileDialog.DirectorySelectedListener() {
+                //  public void directorySelected(File directory) {
+                //      Log.d(getClass().getName(), "selected dir " + directory.toString());
+                //  }
+                //});
+                //fileDialog.setSelectDirectoryOption(false);
+//                fileDialog.showDialog();
+            }
+        });
+
+//        urlString = "http://api.irangrammy.com/api/v4/en/music/11238-5e570e44ae82f0f77133aad4f7789078";
+        urlString = "http://bmxmuseum.com/image/11212789_10152796929931876_1411937103111212490_n557d66a73f_blowup.jpg";
+        final HashMap<String, String> headers = new HashMap<>();
+        headers.put("token", "12-c5c856982961f5ec59cad075015409f17c2a1b99");
+        headers.put("uuid", "1");
         headers.put("agent", "postman");
+
+        downloadLinkTextView.setText(urlString);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
-                ds = new DownloadService(urlString,headers, ctx, pb,tv);
+                ds = new DownloadService(urlString, path, headers, ctx, progressBar, precentTextview);
                 btnPause.setEnabled(true);
             }
         });
@@ -51,5 +130,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*******************************************/
+        /*				Broadcast Receiver		   */
+        /*******************************************/
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("ir.hamed_gh.download.DOWNLOAD_STARTED");
+        filter.addAction("ir.hamed_gh.download.DOWNLOAD_FINISHED");
+        filter.addAction("ir.hamed_gh.download.DOWNLOAD_CANCELLED");
+        BroadcastReceiver mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals("ir.hamed_gh.download.DOWNLOAD_STARTED")) {
+                    Toast.makeText(ctx, "Download started!", Toast.LENGTH_SHORT).show();
+//                    progressBar.setVisibility(View.VISIBLE);
+                    btnStart.setEnabled(false);
+                }
+                if (intent.getAction().equals("ir.hamed_gh.download.DOWNLOAD_FINISHED")) {
+                    Toast.makeText(ctx, "Download finished!", Toast.LENGTH_SHORT).show();
+                    Log.d("status", "Download finished");
+//                    progressBar.setVisibility(View.INVISIBLE);
+                }
+                if (intent.getAction().equals("ir.hamed_gh.download.DOWNLOAD_CANCELLED")) {
+                    Toast.makeText(ctx, "Download cancelled!", Toast.LENGTH_SHORT).show();
+                    Log.d("status", "Download cancelled");
+//                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        };
+        mLocalBroadcastManager.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_DIRECTORY) {
+            if (resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
+                path = data.getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR);
+                pathTextview.setText(path);
+            } else {
+                // Nothing selected
+            }
+        }
     }
 }
